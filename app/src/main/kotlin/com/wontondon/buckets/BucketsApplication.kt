9acacfd7fdd3fe6591b2d25517162c216f6f4e03
Donnie McNeal <donnie@theexperienceengine.com>
2016-02-06 14:30:26 -0800

    Adding timber

--- app/src/main/kotlin/com/wontondon/buckets/BucketsApplication.kt
+++ app/src/main/kotlin/com/wontondon/buckets/BucketsApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import com.wontondon.buckets.presentation.di.components.ApplicationComponent
 import com.wontondon.buckets.presentation.di.components.DaggerApplicationComponent
 import com.wontondon.buckets.presentation.di.modules.ApplicationModule
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 /**
  * @author Donnie McNeal, Jr. (donnie.mcneal@gmail.com)
@@ -15,9 +17,28 @@ class BucketsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        initializeLogging()
+        initializeDagger()
+    }
+
+    private fun initializeLogging() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+        else {
+            // Configure a production logging option (i.e. Crashlytics)
+        }
+    }
+
+    private fun initializeDagger() {
+        Timber.d("Initializing Dagger ApplicationComponent")
 
         this.component = DaggerApplicationComponent.builder()
             .applicationModule(ApplicationModule(this))
             .build()
+
+        Timber.d("Dagger ApplicationComponent initialized")
     }
+
+
 }
